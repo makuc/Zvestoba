b869d219ba1f07a34dc7f4b9eb6b332e12176f4b
Armin Makovec <am4531@student.uni-lj.si>
2017-10-16 16:07:21 +0200

    Začetek implementacije servleta

---
+++ src/main/java/PrviJdbcServlet.java
@@ -0,0 +1,22 @@
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+public class PrviJdbcServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // implementacija
+
+
+    }
+}
