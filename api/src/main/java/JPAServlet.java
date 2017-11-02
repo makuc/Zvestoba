9ddc61eea18817390f51dfaefd3d074feb206df8
Armin Makovec <am4531@student.uni-lj.si>
2017-11-02 18:49:00 +0100

    Izpis vseh uporabnikov iz baze dela praivlno

--- api/src/main/java/JPAServlet.java
+++ api/src/main/java/JPAServlet.java
@@ -25,10 +25,10 @@ public class JPAServlet extends HttpServlet {
 
         writer.append("Hello World\n");
 
-        /*
+
         for(int i=0; i<uporabniki.size(); i++)
         writer.append(uporabniki.get(i).getIme());
-        */
+        
 
     }
 }
